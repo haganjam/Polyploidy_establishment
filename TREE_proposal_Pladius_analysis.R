11f182a08b10930a8091342b4d36ae0f9f2de50e
James Hagan <james_hagan@outlook.com>
2023-06-19 20:29:23 +0200

    update the cleaning of the pladias data

--- TREE_proposal_Pladius_analysis.R
+++ TREE_proposal_Pladius_analysis.R
@@ -4,36 +4,80 @@
 # Title: Examine trait-ploidy correlations from Pladius data for the TREE proposal
 
 # load relevant libraries
-library(here)
 library(dplyr)
 library(ggplot2)
 library(readr)
 library(tidyr)
-library(viridis)
 
 # load the relevant functions
-source(here("Function_plotting_theme.R"))
+source("Function_plotting_theme.R")
 
 # load the Pladius data
-pdat <- read_delim(here("Data/2022-01-16-Pladias-trait-export-for-Wilhelm-Osterman.csv"),
+pdat <- read_delim("Data/2022-01-16-Pladias-trait-export-for-Wilhelm-Osterman.csv",
                    delim = ";", skip = 4)
-View(pdat)
+head(pdat)
 
 # get the relevant columns
+names(pdat)
 pdat <- 
   pdat %>%
-  select(lat_name, pladias_id, lft, rgt, rank, synoecious, monoecious, dioecious) %>%
-  filter(!is.na(synoecious)) %>%
+  select(lat_name, pladias_id, lft, rgt, rank, 
+         synoecious, monoecious, dioecious, gynomonoecious, andromonoecious,
+         gynodioecious, androdioecious, trioecious, trimonoecious) %>%
   filter(rank == "Species")
 
-# how many synoecious, monoecious and dioecious taxa?
-lapply(pdat[, c("synoecious", "monoecious", "dioecious")], function(x) sum(x))
-View(pdat)
+# convert to the long-format
+pdat <- 
+  pdat %>%
+  pivot_longer(cols = contains("ecious"),
+               names_to = "sexual_system",
+               values_to = "TF")
 
+# check if any species have all FALSE sexual systems
+pdat <- 
+  pdat %>%
+  group_by(lat_name, pladias_id, lft, rgt, rank) %>%
+  mutate(sum_TF = sum(TF)) %>%
+  ungroup() %>%
+  filter(!is.na(sum_TF)) %>%
+  select(-sum_TF)
+
+# convert back to wide format now that we're sure we removed all NA vals
+pdat <- 
+  pdat %>%
+  pivot_wider(names_from = "sexual_system",
+              values_from = "TF")
+
+# get a matrix of true false values for the sexual systems
+pdat_sex <- pdat[, -c(1:5)]
+
+# get the identifier matrix
+pdat <- pdat[, c(1:5)]
+
+# calculate the number of sexual systems
+n_sex <- rowSums(pdat_sex)
+
+# make an output vector to collec the sexual systems
+output_sex <- vector(length = length(n_sex))
+for(i in 1:length(n_sex)) {
+
+  # get the correct row
+  y <- pdat_sex[i,]
+  
+  # decide on the sexual systems
+  output_sex[i] <- ifelse(n_sex[i] == 1, names(y[, which(y == TRUE)]), "variable")
+  
+}
+
+# add the data together again
+pdat <- tibble( cbind(pdat, pdat_sex) )
+
+# add the new variables
+pdat$sexual_system <- output_sex
+pdat$n_sexual_system <- n_sex
 
 # load the ploidy level data
-pdat2 <- read_delim(here("Data/Pladias_ploidy_data.csv"),
-                   delim = ",")
+pdat2 <- read_delim("Data/Pladias_ploidy_data.csv", delim = ",")
 
 # rename the first column
 pdat2 <- 
@@ -45,62 +89,42 @@ pdat2 <-
   pdat2 %>%
   filter(!is.na(Polyploidy_yes_no) )
 
-
 # join the ploidy data to the trait data
 pdat2 <- left_join(pdat2, pdat, by = c("lat_name", "pladias_id"))
 head(pdat2)
 
-# remove the NAs in the synoecious column
+# remove the NAs the dataset where we couldn't classify a sexual system
 pdat2 <- 
   pdat2 %>%
-  filter(!is.na(synoecious))
+  filter(!is.na(sexual_system))
 
-# convert the reproductive mode columns into the long format
+# check which types of species are left
+unique(pdat2$`Origin in the Czech Republic`)
+sum(is.na(pdat2$`Origin in the Czech Republic`))
+
+# remove the species with unknown origin
 pdat2 <- 
   pdat2 %>%
-  pivot_longer(cols = c("synoecious", "dioecious", "monoecious"),
-               names_to = "reproductive_mode",
-               values_to = "value") %>%
-  filter(value == TRUE) %>%
-  select(-value)
+  filter(!is.na(`Origin in the Czech Republic`))
 
-
-# plot ploidy class and reproductive mode
-
-# collapse the 6 and 7 ploidy levels with the 5 level
-pdat2$Ploidy_class1 <- ifelse(pdat2$Ploidy_class1 %in% c(5, 6, 7), 5, pdat2$Ploidy_class1)
-
-# re-level the factors
-pdat2$Ploidy_class_fac <- factor(pdat2$Ploidy_class1)
-levels(pdat2$Ploidy_class_fac) <- paste(1:5, "x", sep = "")
-
+# reorganise the columns
+names(pdat2)
 pdat2 <- 
   pdat2 %>%
-  group_by(reproductive_mode) %>%
-  summarise(proportion = sum(Polyploidy_yes_no)/n(),
-            n = n(), 
-            .groups = "drop")
+  rename(ploidy_level_x = Ploidy_level_x,
+         ploidy_max = Ploidy_max,
+         ploidy_class_1 = Ploidy_class1,
+         polyploidy_yn = Polyploidy_yes_no,
+         origin = `Origin in the Czech Republic`,
+         pollination_syndrome = Pollination_syndrom_grouped) %>%
+  select(lat_name, pladias_id, lft, rgt, rank, 
+         ploidy_level_x, ploidy_max, ploidy_class_1, polyploidy_yn,
+         origin, pollination_syndrome,
+         contains("ecious"),
+         sexual_system, n_sexual_system) %>%
+  arrange(lat_name)
 
-p1 <- 
-  ggplot(data = pdat2,
-         mapping = aes(x = reproductive_mode, y = proportion)) +
-  geom_col(width = 0.2, colour = viridis(n = 1, begin = 0.5, end = 0.5),
-           fill = viridis(n = 1, begin = 0.5, end = 0.5)) +
-  scale_y_continuous(limits = c(0, 0.55), breaks = c(0.1, 0.2, 0.3, 0.4, 0.5)) +
-  annotate(geom = "text", label = paste("N = ", pdat2$n[1], sep = "" ),
-           x = 1, y = 0.35) + 
-  annotate(geom = "text", label = paste("N = ", pdat2$n[2], sep = "" ),
-           x = 2, y = 0.21) +
-  annotate(geom = "text", label = paste("N = ", pdat2$n[3], sep = "" ),
-           x = 3, y = 0.52) + 
-  xlab("Reproductive mode") +
-  ylab("Polyploid species prop.") +
-  theme_meta()
-  
-p1
-
-ggsave(filename = here("Figures/fig_1c.png"), 
-       plot = p1, width = 12, height = 11, dpi = 300,
-       units = "cm")
+# write this into a .csv file
+write_csv(x = pdat2, file = "Data/pladias_sexual_ploidy_data.csv")
 
 ### END
