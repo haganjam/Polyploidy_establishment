03859e30f437d93e884d4ed5359b62dd69a2da32
haganjam <james_hagan@outlook.com>
2025-01-21 14:56:52 +0100

    change the y-axis label for fig 3.

--- code/e_combine_fig3.R
+++ code/e_combine_fig3.R
@@ -1,14 +1,25 @@
 
-# Combine Fig. 2a and Fig. 2b
+# Combine Fig. 3a and Fig. 3b
 
 # load relevant libraries
+library(tidyverse)
 library(here)
 library(ggplot2)
 library(ggpubr)
 
+# remotes::install_version(package = "ggplot2", version = "3.4.4")
+
+# check ggplot2 version
+packageVersion("ggplot2")
+
 # load the plot data
 load(here("figures_tables", "fig3a.RData"))
 load(here("figures_tables", "fig3b.RData"))
+
+# change the title of p1
+p1 <-
+  p1 +
+  ylab("Minority cytotype frequency")
 
 # combine these figures using ggarrange
 p12 <- 
@@ -25,6 +36,10 @@ ggsave( filename = here("figures_tables/fig3.png"),
 
 ggsave( filename = here("figures_tables/fig3.tiff"), 
         plot = p12, width = 20, height = 10.5, dpi = 800,
+        units = "cm")
+
+ggsave( filename = here("figures_tables/fig3.pdf"), 
+        plot = p12, width = 20, height = 10.5,
         units = "cm")
 
 ### END
